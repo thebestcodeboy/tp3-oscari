e9674befac968038c5e9e15cc47022b9ca5cdb79
thebestcodeboy <oscarilucio4@gmail.com>
2025-09-17 23:30:51 -0300

    TP2 React + File API

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ImageUploader from "./components/ImageUploader";
+
+function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>TP2 - File API</h1>
+      <ImageUploader />
+    </main>
+  );
+}
+
+export default App;
