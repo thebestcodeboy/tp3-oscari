68bcd9fc978e1e390d40a478d1a0ffd5ded67c7b
thebestcodeboy <oscarilucio4@gmail.com>
2025-09-25 19:49:56 -0300

    Fix Pages: base + 404

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tp3-oscari/',   // exacto al nombre de tu repo
+  base: '/tp3-oscari/',   // exacto al nombre dell repo
 })
